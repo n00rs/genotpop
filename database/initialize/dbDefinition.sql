03c84470d9d8beda9b640efbedcb9539cc096af0
harshad_bs <harshad>
2025-03-19 00:44:09 +0530

    [hbs_branch] db defnition added

---
+++ database/initialize/dbDefinition.sql
@@ -0,0 +1,113 @@
+CREATE TABLE IF NOT EXISTS tbl_user (
+  pk_bint_user_id SERIAL PRIMARY KEY NOT NULL,
+  vchr_email VARCHAR(100) UNIQUE NOT NULL,
+  vchr_name VARCHAR(100) NOT NULL,
+  vchr_password TEXT NOT NULL, -- Store hashed password
+  vchr_role VARCHAR(20) NOT NULL,
+  bln_privileged_user BOOLEAN DEFAULT FALSE,  -- no use
+  vchr_phone VARCHAR(20),
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  vchr_acc_private_key TEXT,
+  vchr_acc_public_key TEXT,
+  vchr_refr_private_key TEXT,
+  vchr_refr_public_key TEXT,
+  chr_document_status CHAR(1) DEFAULT 'N'
+);
+CREATE INDEX idx_user_email ON tbl_user(vchr_email);
+
+CREATE TABLE IF NOT EXISTS tbl_category (
+  pk_bint_category_id SERIAL PRIMARY KEY NOT NULL,
+  vchr_category_code VARCHAR(100) UNIQUE NOT NULL,
+  vchr_category_name VARCHAR(100) NOT NULL,
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  chr_document_status CHAR(1) DEFAULT 'N'
+);
+CREATE INDEX idx_category_code_and_name ON tbl_category(vchr_category_code, vchr_category_name);
+
+CREATE TABLE IF NOT EXISTS tbl_stock (
+  pk_bint_stock_id SERIAL PRIMARY KEY NOT NULL,
+  vchr_stock_code VARCHAR(100) UNIQUE NOT NULL,
+  vchr_stock_name VARCHAR(100) NOT NULL,
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  chr_document_status CHAR(1) DEFAULT 'N'
+);
+CREATE INDEX idx_stock_code_and_name ON tbl_stock(vchr_stock_code, vchr_stock_name);
+
+CREATE TABLE IF NOT EXISTS tbl_customer (
+  pk_bint_customer_id SERIAL PRIMARY KEY NOT NULL,
+  vchr_customer_code VARCHAR(100) UNIQUE NOT NULL,
+  vchr_customer_name VARCHAR(255) NOT NULL,
+  vchr_phone VARCHAR(20) UNIQUE NOT NULL,
+  vchr_email VARCHAR(100) UNIQUE,
+  vchr_address TEXT,
+  dbl_discount DECIMAL(10,2) DEFAULT 0.00,
+  vchr_gst_no VARCHAR(20) UNIQUE, 
+  vchr_gst_address TEXT,
+  dbl_outstanding DECIMAL(10,2) DEFAULT 0.00 NOT NULL,
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  chr_document_status CHAR(1) DEFAULT 'N'
+);
+CREATE INDEX idx_customer_code_and_name ON tbl_customer(vchr_customer_code, vchr_customer_name);
+
+
+CREATE TABLE IF NOT EXISTS tbl_inventory (
+  pk_bint_inventroy_id SERIAL PRIMARY KEY NOT NULL,
+  batchcode VARCHAR(100) UNIQUE NOT NULL,
+  fk_stock_id INTEGER REFERENCES tbl_stock(pk_bint_stock_id) ON DELETE CASCADE,
+  fk_category_id INTEGER REFERENCES tbl_category(pk_bint_category_id) ON DELETE CASCADE,
+  cost DECIMAL(10,2) NOT NULL CHECK (cost >= 0),
+  unit_price DECIMAL(10,2) NOT NULL CHECK (unit_price >= 0),
+  quantity INT NOT NULL CHECK (quantity >= 0),
+  expiry DATE NOT NULL,
+  chr_document_status CHAR(1) DEFAULT 'N',
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id)
+);
+
+CREATE TABLE IF NOT EXISTS tbl_sales_master (
+  pk_bint_sales_master_id SERIAL PRIMARY KEY NOT NULL,
+  vchr_document_number VARCHAR(50) UNIQUE NOT NULL,
+  dat_document_date DATE NOT NULL,
+  vchr_refference VARCHAR (200),
+  fk_bint_customer_id INTEGER NOT NULL REFERENCES tbl_customer(pk_bint_customer_id) ON DELETE CASCADE,  
+  gst_percentage DECIMAL(5,2) NOT NULL CHECK (gst_percentage >= 0 AND gst_percentage <= 100),
+  dble_before_vat DECIMAL(10,2) NOT NULL CHECK (dble_before_vat >= 0),
+  dbl_total_amount DECIMAL(10,2) NOT NULL, 
+  chr_document_status CHAR(1) DEFAULT 'N',
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id),
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id)
+);
+
+CREATE TABLE IF NOT EXISTS tbl_sales_master_details (
+  pk_bint_sales_master_details_id SERIAL PRIMARY KEY,
+  fk_bint_sales_master_id INTEGER NOT NULL REFERENCES tbl_sales_master(pk_bint_sales_master_id) ON DELETE CASCADE,
+  fk_bint_category_id INTEGER NOT NULL REFERENCES tbl_category(pk_bint_category_id) ON DELETE CASCADE,
+  fk_bint_stock_id INTEGER NOT NULL REFERENCES tbl_stock(pk_bint_stock_id) ON DELETE CASCADE,
+  fk_bint_inventory INTEGER REFERENCES tbl_inventory(pk_bint_inventory_id) ON DELETE SET NULL,
+  dbl_discount DECIMAL(10,2) DEFAULT 0.00 CHECK (dbl_discount >= 0),
+  unit_price DECIMAL(10,2) NOT NULL CHECK (unit_price >= 0),
+  reference TEXT,
+  dbl_quantity DECIMAL(10,2) NOT NULL CHECK (dbl_quantity >= 0),
+  dbl_total_amount DECIMAL(10,2) NOT NULL CHECK (dbl_total_amount >= 0),
+  chr_document_status CHAR(1) DEFAULT 'N' CHECK (chr_document_status IN ('N', 'Y')),
+  dat_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  dat_modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  fk_bint_created_id INTEGER REFERENCES tbl_user(pk_bint_user_id) ON DELETE SET NULL,
+  fk_bint_modified_id INTEGER REFERENCES tbl_user(pk_bint_user_id) ON DELETE SET NULL
+);
