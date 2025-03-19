89eb1023e76a8e199fbd7b2c9944266e5e295cb2
harshad_bs <harshad>
2025-03-19 15:24:14 +0530

    [hbs_branch] reset password completed

--- database/initDb.ts
+++ database/initDb.ts
@@ -5,20 +5,20 @@ import process  from "process";
 
 process.loadEnvFile(".env")
 const executeSQLFile = async () => {
-  // Get a PostgreSQL connection objMainDbConnection  
-  const objMainDbConnection = await getPgConnection({ blnPool: true });
+  // Get a PostgreSQL connection objConnectionPool  
+  const objConnectionPool = await getPgConnection({ blnPool: true });
 
   try {
     const __dirname = path.resolve("database", "initialize", "dbDefinition.sql");  
     const sqlQueries = fs.readFileSync(__dirname, "utf-8");
     // Execute SQL queries
-    await objMainDbConnection.query(sqlQueries);
+    await objConnectionPool.query(sqlQueries);
 
     console.log("Database initialized successfully!");
   } catch (error) {
     console.error("Error executing SQL file:", error);
   } finally {
-    await objMainDbConnection.end();
+    await objConnectionPool.end();
   }
 };
 
--- libs/authentication/index.ts
+++ libs/authentication/index.ts
@@ -1,17 +1,6 @@
 import { signUpController } from "./signup.controller.ts";
 import { loginController } from "./login.controller.ts";
-
-// const loginController = async({ body, ...source }) => {
-//   console.log({ body, source });
-//   return {}
-// };
-
- 
-
-const resetPasswordController = async({ body, ...source }) => {
-  console.log({ body, source });
-  return {}
-};
+import { resetPasswordController } from "./resetPassword.controller.ts";
 
 const refreshTokenController = async({ body, ...source }) => {
   console.log({ body, source });
